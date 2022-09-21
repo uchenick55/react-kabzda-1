55d2256b1cbfc5526e1157146dba33f774497e47
uchenick55 <uchenick55@yandex.ru>
2022-09-21 22:46:46 +0300

    Login ReduxForm UI render

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import {Field, reduxForm} from 'redux-form'
+import React from "react";
+import {Field, reduxForm} from "redux-form";
+import classes from './Login.module.css'
 
-let LoginForm = (props) => {
+const LoginForm = props => {
+    const {handleSubmit} = props;
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={handleSubmit}>
             <div>
-                <Field name={"login"} component={"input"} placeholder={"login"}/>
-            </div>
-            <div>
-                <Field name={"password"} component={"input"} placeholder={"password"}/>
-            </div>
-            <div>
-                <Field name={"rememberMe"} component={"input"} type={"checkbox"}/>
-            </div>
-            <div>
-                <button>Submit</button>
+                <div>
+                    <Field
+                        name="login"
+                        component = "input"
+                        type = "text"
+                        placeholder = "login"
+                    />
+                </div>
+                <div>
+                    <Field
+                        name="password"
+                        component = "input"
+                        type = "text"
+                        placeholder = "password"
+                    />
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
             </div>
         </form>
     )
 }
 
-let LoginReduxForm = reduxForm({form: 'contact'})(LoginForm)
+let LoginReduxForm=reduxForm({form: 'login'})(LoginForm)
 
 let onSubmit = (formData) => {
-    return (
-        console.log(formData)
-    )
+    alert(formData)
 }
 
 let Login = () => {
-    return (<div>
-            <h1>Login</h1>
-            <LoginReduxForm onSubmit={onSubmit}/>
+    return(
+        <div className={classes.loginCommon}>
+            <h2>Войдите в аккаунт</h2>
+            <LoginReduxForm onSubmit={onSubmit} />
         </div>
+
     )
 }
+
 export default Login
