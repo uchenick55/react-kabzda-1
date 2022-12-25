8ca3bf9aa2eca765f3ce1b9d83cad0fb95d85496
uchenick55 <uchenick55@yandex.ru>
2022-12-25 12:21:29 +0300

    тестовая отправка фидбека на 2 почты

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {HashRouter} from "react-router-dom";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import NavBarContainer from "./components/Navbar/NavBarContainer";
 import {connect} from "react-redux";
-import {initialisedAppThunkCreator, postFeedBackThunkCreator} from "./redux/app-reducer";
+import {initialisedAppThunkCreator} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import InfoContainer from "./components/Info/InfoContainer";
 import ContentContainer from "./components/Content/ContentContainer";
@@ -13,7 +13,6 @@ import FeedBack from "./components/FeedBack/FeedBack";
 class App extends React.Component { // конвертируем app в классовую компоненту для жизненного цикла
     componentDidMount() {
         this.props.initialisedAppThunkCreator() // запускаем инициализацию приложения
-     //   this.props.postFeedBackThunkCreator()
     }
 
     render() {
@@ -54,13 +53,5 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, {initialisedAppThunkCreator, postFeedBackThunkCreator})(App);
+export default connect(mapStateToProps, {initialisedAppThunkCreator})(App);
 // коннектим к app флаг и санки инициализации
-
-
-
-
-
-
-
-
--- src/components/Info/FeedBackInfo.jsx
+++ src/components/Info/FeedBackInfo.jsx
@@ -1,6 +1,11 @@
 let FeedBackInfo = () => {
     return (<div>
         <p>На этой странице можно сообщить о найденом баге, написать пожелания или просто сказать привет :)</p>
+        <p>Проверить отправку почты можно на тестовом почтовом ящике {" "}
+            <a href="https://passport.yandex.ru/auth/">яндекс почты</a>: <br/>
+            testfeedback2023 <br/>
+            testfeedback2023_password <br/>
+        </p>
     </div>)
 }
 
