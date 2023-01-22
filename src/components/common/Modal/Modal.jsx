2d72318030dc318271832faaefc61f7d129408f8
uchenick55 <uchenick55@yandex.ru>
2023-01-22 15:27:10 +0300

    закрыть модальное окно по escape и потере фокуса

--- src/components/common/Modal/Modal.jsx
+++ src/components/common/Modal/Modal.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useCallback, useEffect} from 'react'
 import classes from './Modal.module.css'
 
 const Modal = (props) => {
+    const escFunction = useCallback((event)=>{
+        if (event.key==="Escape") {
+            // сделать что делает escape
+            props.onClose()
+        }
+    },[])
+
+    useEffect(()=>{
+        document.addEventListener("keydown", escFunction, false)
+        return () => {
+
+            document.addEventListener("keydown", escFunction, false)
+        }
+    }, [])
+
     if (!props.show) {
         return null
     }
+
     return (
-        <div className={classes.modal}>
-            <div className={classes.modal_content}>
+        <div className={classes.modal} onClick={props.onClose}> {/*по клику вне окна закрыть модальное окно*/}
+            <div className={classes.modal_content}
+                 onClick={(e)=>{e.stopPropagation()}}>
+                {/*при клике по самому окну, его не закрывать*/}
                 <div className={classes.modal_header}>
                     <h4 className={classes.modal_title}>Modal Title</h4> {/*заголовок модального окна*/}
                 </div>
