aa46ff47ba763198084c3c42cb3fed4c7b9bc4a1
uchenick55 <uchenick55@yandex.ru>
2023-02-22 17:13:03 +0300

    растынул фон на всесь эран

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import store from "./redux/store-redux"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import AppDayNightContainer from "./AppDayNightContainer";
-import reportWebVitals from './reportWebVitals';
+/*
+import './-index.css';
+*/
+import reportWebVitals from './-reportWebVitals';
 import {Provider} from "react-redux";
+import AppBS from "./AppBS";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <AppDayNightContainer />
+            <AppBS />
         </Provider>
     </React.StrictMode>
 );
