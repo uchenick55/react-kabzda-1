26e2ffdbca086097f18d9d7140a3e4d4eb89c3b8
uchenick55 <uchenick55@yandex.ru>
2023-04-23 15:39:58 +0300

    PaginationByCourse.tsx

--- src/components/common/Pagination/PaginationByCourseBS.tsx
+++ src/components/common/Pagination/PaginationByCourseBS.tsx
@@ -2,36 +2,45 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Pagination from 'react-bootstrap/Pagination';
 import classes from "./Pagination.module.css"
+type PaginationByCourseType = {
+    totalUsersCount: number,
+    pageSize:number,
+    currentPage:number,
+    onPageChanged:(setPage:number)=>void,
+    currentRangeLocal: number,
+    onChangeRangeLocal:(rangeShift:number) =>void,
 
-let PaginationByCourse = ({
+}
+const PaginationByCourse:React.FC<PaginationByCourseType> = ({
                               totalUsersCount, // общее число пользователей на сервере
                               pageSize, // количество пользователей на одной странице
                               currentPage, // текущая страница пагинации
                               onPageChanged, // колбек-функция изменения текущей страницы
-                              PortionSize = 6, // количество отображаемых страниц из всего массива
                               currentRangeLocal,
                               onChangeRangeLocal
                           }) => {
+    const PortionSize:number = 6 // количество отображаемых страниц из всего массива
     // currentRange - текущий диапазон. Он в PortionSize меньше PagesCount
     //setCurrentRange - изменение currentRange по клику на кнопку
     // PortionSizeLeft - Нижнее значение порций (не меньше 1)
    // const [mouseHovered, setMouseHovered] = useState("");
-    let PagesCount = Math.ceil(totalUsersCount / pageSize); // сколько всего страниц можно вызвать
+    const PagesCount = Math.ceil(totalUsersCount / pageSize); // сколько всего страниц можно вызвать
     //с пользователями пачками по PageSize
-    let pages = []; // определяем массив страниц под всех пользователей
+    const pages = []; // определяем массив страниц под всех пользователей
     for (let i = 1; i <= PagesCount; i++) {
         // В этот массив
         pages.push(i); // добавляем все страницы пользователей
     }
 
-    let PortionSizeLeft = 1 + PortionSize * (currentRangeLocal - 1); // Нижнее значение порций (не меньше 1)
-    let PortionSizeRight = PortionSize * currentRangeLocal; // Верхнее значение страниц (не больше PagesCount)
-    let slicedPages2 = pages.filter(
+    const PortionSizeLeft = 1 + PortionSize * (currentRangeLocal - 1); // Нижнее значение порций (не меньше 1)
+    const PortionSizeRight = PortionSize * currentRangeLocal; // Верхнее значение страниц (не больше PagesCount)
+    const slicedPages2 = pages.filter(
         // фильтруем весь массив страниц пользователей
         (p) => p >= PortionSizeLeft && p <= PortionSizeRight // оставляем только в заданном диапазоне
     );
 
-    const setPortion = (setPortionValue) => { // задать текущую порцию пагинации
+    type setPortionValueType = "prevPortion" | "nextPortion"
+    const setPortion = (setPortionValue:setPortionValueType) => { // задать текущую порцию пагинации
         if (setPortionValue === "prevPortion" && currentRangeLocal > 1) // если мы жмем prevPortion
         {
             onChangeRangeLocal(-1) // уменьшаем диапазон на 1
@@ -42,7 +51,7 @@ let PaginationByCourse = ({
         }
     };
 
-    let renderSlicedPages = slicedPages2.map((p) => { // мапинг отобранного массива
+    const renderSlicedPages = slicedPages2.map((p) => { // мапинг отобранного массива
         return (
             <Pagination.Item // пагинация бутстрапа
                 active={p === currentPage} // акттивная страница
@@ -61,7 +70,7 @@ let PaginationByCourse = ({
         <div className={classes.pagination}>
             <Pagination className={"pagination align-items-center justify-content-center"} > {/*стиль мышки рука */}
                 <Pagination.Prev onClick={()=>{setPortion("prevPortion")}} /> {/*диапазон пагинации вниз*/}
-                {renderSlicedPages} {/*отрисовка пагинации десяти страниц внутри кнопок*/}
+                {renderSlicedPages} {/*отрисовка пагинации страниц внутри кнопок*/}
                 <Pagination.Next onClick={()=>{setPortion("nextPortion")}}/>  {/*диапазон пагинации вверх*/}
             </Pagination>
         </div>
